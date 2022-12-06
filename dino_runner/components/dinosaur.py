54c4c1ad455236a7c4e585fa3662c8904467dea3
TheRohan64 <Rohan.Escobar@fundacion-jala.org>
2022-12-06 01:50:01 -0400

    Changes_0

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,10 +1,11 @@
 from pygame.sprite import Sprite
 import pygame
 
-from dino_runner.utils.constants import JUMPING, RUNNING
+from dino_runner.utils.constants import JUMPING, RUNNING, DUCKING
 X_POS = 80
 Y_POS = 310
-JUMP_VELOCITY = 8
+Y_POS_DUCK = 340
+JUMP_VELOCITY = 6
 class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
@@ -15,9 +16,11 @@ class Dinosaur(Sprite):
 
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.jump_velocity = JUMP_VELOCITY
 
     def update(self, user_input):
+
         if self.dino_run:
             self.run()
 
@@ -27,10 +30,17 @@ class Dinosaur(Sprite):
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+            self.dino_duck = False
 
-        elif not self.dino_jump:
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_jump = False
+            self.dino_run = False
+            self.dino_rect = True
+
+        elif not self.dino_jump or not self.dino_duck:
             self.dino_jump = False
             self.dino_run = True
+            self.dino_duck = False
 
         if self.step_index >= 8:
             self.step_index = 0
@@ -38,13 +48,23 @@ class Dinosaur(Sprite):
     def jump(self):
         self.image = JUMPING
         self.dino_rect.y -= self.jump_velocity * 4
-        self.jump_velocity -= 0.8
+        self.jump_velocity -= 0.6
 
         if self.jump_velocity < -JUMP_VELOCITY:
             self.dino_jump = False
             self.dino_rect.y = Y_POS
             self.jump_velocity = JUMP_VELOCITY
-        print(self.jump_velocity)
+
+    def duck(self):
+        self.image = DUCKING[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS_DUCK
+        if self.step_index > 4:
+            self.image = DUCKING[1]
+        else:
+            self.image = DUCKING[0]
+        self.step_index += 1
 
     def run(self):
         self.dino_rect = self.image.get_rect()
