2b3e1c09ffa037e31cec63b9203fa24629b0f134
TheRohan64 <Rohan.Escobar@fundacion-jala.org>
2022-12-07 18:03:05 -0400

    class 2

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,7 @@
 import pygame
 
 from dino_runner.components.dinosaur import Dinosaur
-from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, SMALL_CACTUS, TITLE, FPS
 
@@ -18,7 +18,7 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         self.player = Dinosaur()
-        self.obstacle_manager = []
+        self.obstacle_manager = ObstacleManager
 
     def run(self):
         # Game loop: events - update - draw
@@ -37,7 +37,7 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
-        self.obstacle_manager.update(self.game_speed)
+        self.obstacle_manager.update(self.game)
 
     def draw(self):
         self.clock.tick(FPS)
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,3 +1,4 @@
+import pygame
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.utils.constants import SMALL_CACTUS
 
@@ -5,12 +6,16 @@ class ObstacleManager():
     def __init__(self):
         self.obstacles = []
 
-    def update(self, game_speed):
+    def update(self, game):
         if len(self.obstacles) == 0:
             self.obstacles.append(Cactus[SMALL_CACTUS])
 
         for obstacle in self.obstacles:
-            obstacle.update(game_speed)
+            obstacle.update(game.game_speed, self.obstacles)
+            if game.player.dino_rect.colliderect(obstacle.rect):
+                pygame.time.delay(1000)
+                game.playing = False
+                break
 
     def draw(self, screen):
         for obstacle in self.obstacles:
