15344a5204457c960c2c31e4b3f7962aaf9c4712
TheRohan64 <Rohan.Escobar@fundacion-jala.org>
2022-12-07 17:30:01 -0400

    Update game.py

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,9 +1,9 @@
 import pygame
 
 from dino_runner.components.dinosaur import Dinosaur
-from dino_runner.components.obstacles import cactus
+from dino_runner.components.obstacles.cactus import Cactus
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, SMALL_CACTUS, LARGE_CACTUS, BIRD, TITLE, FPS
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, SMALL_CACTUS, TITLE, FPS
 
 
 class Game:
@@ -18,7 +18,7 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         self.player = Dinosaur()
-        self.obstacle = [cactus(SMALL_CACTUS)]
+        self.obstacle = [Cactus(SMALL_CACTUS)]
 
     def run(self):
         # Game loop: events - update - draw
@@ -37,14 +37,14 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
-        self.obstacles[0].update(self.game_speed)
+        self.obstacle[0].update(self.game_speed)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.player.draw(self.screen)
-        self.obstacles[0].draw(self.screen)
+        self.obstacle[0].draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
