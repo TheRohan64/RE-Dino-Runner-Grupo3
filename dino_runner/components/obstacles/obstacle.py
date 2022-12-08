07f85a62c16860b19a100d2b897fe1fc654ac68d
TheRohan64 <Rohan.Escobar@fundacion-jala.org>
2022-12-07 22:56:29 -0400

    Update obstacle.py

--- dino_runner/components/obstacles/obstacle.py
+++ dino_runner/components/obstacles/obstacle.py
@@ -1,14 +1,15 @@
 from pygame.sprite import Sprite
 from dino_runner.components import obstacles
+from dino_runner.utils.constants import SCREEN_WIDTH
 
 class Obstacle(Sprite):
     def __init__(self, images, type):
         self.images = images
         self.type = type
         self.rect = self.images[self.type].get_rect()
-        self.rect.x = 1100
+        self.rect.x = SCREEN_WIDTH
 
-    def update(self, game_speed, obstacles):
+    def update(self, game_speed):
         self.rect.x -= game_speed
         if self.rect.x < -self.rect.width:
             obstacles.pop()
